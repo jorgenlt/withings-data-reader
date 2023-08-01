2af4074065ea5d6e600e709d0cbeedb0c4bf4795
Joergen Tjoennteig <jorgenlt@outlook.com>
2023-08-01 20:02:55 +0200

    upload multiple files to state

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,25 @@
 import Spo2 from './features/dataReader/Spo2'
+import { uploadFilesThunk } from './features/dataReader/dataReaderSlice'
+import { useDispatch } from 'react-redux'
 
 function App() {
+  const dispatch = useDispatch();
+
+  const handleFileUpload = e => {
+    // console.log(e.target.files);
+    // const files = e.target.files;
+
+    // for (const key in files) {
+    //   console.log(files[key]);
+    // }
+    dispatch(uploadFilesThunk(e.target.files));
+  }
+  
+
   return (
     <>
       <Spo2 />
+      <input type="file" multiple="multiple" onChange={handleFileUpload} />
     </>
   )
 }
--- src/features/dataReader/dataReaderSlice.js
+++ src/features/dataReader/dataReaderSlice.js
@@ -8,10 +8,11 @@ const initialState = {
   error: null,
   status: 'idle',
   spo2Auto: [],
-  filteredSpo2Auto: []
+  filteredSpo2Auto: [],
+  files: {}
 }
 
-// Upload thunk
+// Upload thunks
 export const uploadFileThunk = createAsyncThunk(
   'data/uploadFileThunk',
   async file => {
@@ -30,54 +31,110 @@ export const uploadFileThunk = createAsyncThunk(
       return Promise.reject(err);
     });
   }
-);
-
-export const dataReader = createSlice({
-  name: 'dataReader',
-  initialState,
-  reducers: {
-    filterSpo2: (state, action) => {
-      state.filteredSpo2Auto = filterByDate(state.spo2Auto, action.payload)
+  );
+  
+  export const uploadFilesThunk = createAsyncThunk(
+    'data/uploadFilesThunk',
+    async files => {
+      const parsedFiles = [];
+      for (const file in files) {
+        // Check if it is a valid file
+        if (files[file].type === "text/csv") {
+          
+          await new Promise((resolve, reject) => {
+            Papa.parse(files[file], {
+              skipEmptyLines: true,
+              complete: results => {
+                parsedFiles.push(
+                  {
+                    filename: files[file].name, 
+                    data: results
+                  }
+                );
+                resolve(results.data);
+              },
+              error: err => {
+                reject(err);
+              }
+            });
+          })
+          .catch(err => {
+            console.error(err);
+            return Promise.reject(err);
+          });
+        }
+      }
+      return parsedFiles;
     }
-  },
-  extraReducers: builder => {
-    builder
-      .addCase(uploadFileThunk.pending, state => {
-        state.status = 'loading';
-      })
-      .addCase(uploadFileThunk.fulfilled, (state, action) => {
-        state.status = 'succeeded';
-
-        // remove headers
-        action.payload.shift();
-
-        // Creating an array of objects
-        const data = action.payload.map(row => {
-          const start = row[0] ? format(new Date(row[0]), 'MMMM dd yyyy, h:mm aaa') : '';
-          const time = row[0] ? format(new Date(row[0]), 'h:mm aaa') : '';
-          const value = row[2] ? parseInt(row[2].replace(/[[\]]/g, '')) : '';
-
-          return {
-            start,
-            time,
-            value
-          }
-        });
-
-        // Sort by date
-        const sortedData = data.sort((a, b) => new Date(a.start) - new Date(b.start))
+    );
+    
+    
+    
+    
+    
+    export const dataReader = createSlice({
+      name: 'dataReader',
+      initialState,
+      reducers: {
+        filterSpo2: (state, action) => {
+          state.filteredSpo2Auto = filterByDate(state.spo2Auto, action.payload)
+        }
+      },
+      extraReducers: builder => {
+        builder
+        .addCase(uploadFileThunk.pending, state => {
+          state.status = 'loading';
+        })
+        .addCase(uploadFileThunk.fulfilled, (state, action) => {
+          state.status = 'succeeded';
+          
+          // remove headers
+          action.payload.shift();
+          
+          // Creating an array of objects
+          const data = action.payload.map(row => {
+            const start = row[0] ? format(new Date(row[0]), 'MMMM dd yyyy, h:mm aaa') : '';
+            const time = row[0] ? format(new Date(row[0]), 'h:mm aaa') : '';
+            const value = row[2] ? parseInt(row[2].replace(/[[\]]/g, '')) : '';
+            
+            return {
+              start,
+              time,
+              value
+            }
+          });
+          
+          // Sort by date
+          const sortedData = data.sort((a, b) => new Date(a.start) - new Date(b.start))
+          
+          // Updating spo2Auto in state
+          state.spo2Auto = sortedData;
+          state.filteredSpo2Auto = filterByDate(sortedData, new Date(sortedData[sortedData.length - 1].start))
+        })
+        .addCase(uploadFileThunk.rejected, state => {
+          state.status = 'failed';
+        })
         
-        // Updating spo2Auto in state
-        state.spo2Auto = sortedData;
-        state.filteredSpo2Auto = filterByDate(sortedData, new Date(sortedData[sortedData.length - 1].start))
-      })
-      .addCase(uploadFileThunk.rejected, state => {
-        state.status = 'failed';
-      })
-  }
-})
+        
+        // upload multiple files
+        .addCase(uploadFilesThunk.pending, state => {
+          state.status = 'loading';
+        })
+        .addCase(uploadFilesThunk.fulfilled, (state, action) => {
+          const files = action.payload;
 
-// Action creators are generated for each case reducer function
-export const { filterSpo2 } = dataReader.actions
-
-export default dataReader.reducer
+          files.forEach(file => {
+            state.files[file.filename] = file.data.data;
+          });
+          
+        })
+        .addCase(uploadFilesThunk.rejected, state => {
+          state.status = 'failed';
+        });
+      }
+    })
+    
+    // Action creators are generated for each case reducer function
+    export const { filterSpo2 } = dataReader.actions
+    
+    export default dataReader.reducer
